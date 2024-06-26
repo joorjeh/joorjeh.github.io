ba8679d145d5690a22e202ac98a45be6a734fd6a
George Balch <george.balch@proton.me>
2024-06-26 12:49:22 -0700

    Remove hover logic

--- src/Header.tsx
+++ src/Header.tsx
@@ -3,21 +3,8 @@ import XIcon from '@mui/icons-material/X';
 import GitHubIcon from '@mui/icons-material/GitHub';
 import { Link } from "react-router-dom";
 import './Header.css';
-import { useState } from "react";
 
 const Header = () => {
-    const [stateChange, setStateChange] = useState<number>(0);
-
-    const handleMouseEnter = (event: any) => {
-        event.target.style.color = 'black';
-        setStateChange(stateChange + 1); // to force rerender
-    }
-
-    const handleMouseLeave = (event: any) => {
-        event.target.style.color = 'inherit';
-        setStateChange(stateChange + 1); // to force rerender
-    }
-
     return (
         <Flex
             flexDirection='column'
@@ -37,15 +24,11 @@ const Header = () => {
                 <Link to="/">dreamlurch</Link>
                 <Flex gap='10px' marginLeft='auto'>
                     <Link 
-                        onMouseEnter={handleMouseEnter} 
-                        onMouseLeave={handleMouseLeave}
                         to='https://github.com/dreamlurch'
                     >
-                        <GitHubIcon />
+                        <GitHubIcon className="g-icon" />
                     </Link>
                     <Link 
-                        onMouseEnter={handleMouseEnter} 
-                        onMouseLeave={handleMouseLeave}
                         to='https://x.com/dreamlurch'
                     >
                         <XIcon />
