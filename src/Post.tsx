716c6391c5b9330ccb1b15d4d7f67dbf0c65b2a6
George Balch <george.balch@proton.me>
2024-07-06 19:33:15 -0700

    TODO

--- src/Post.tsx
+++ src/Post.tsx
@@ -2,6 +2,7 @@ import Markdown from "markdown-to-jsx";
 import { useLocation } from "react-router-dom";
 import './Post.css';
 
+// TODO add modifier for <code> blocks in Markdown with custom component
 const Post = () => {
   const location = useLocation();
   const { content } = location.state;
